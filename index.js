75f67032e9c904bcc6272fb9a1d96b29beeb1508
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2020-01-08 15:16:28 +0900

    완성

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ window.addEventListener("load", function(){
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://220.85.155.13:5187/categories', true);
+
     xhr.onload = function() {
         if(this.status === 200) {
+
             categories = JSON.parse(this.responseText);
-            console.log("categories 출력", categories);
+            // console.log("categories 출력", categories.categoryResponses[1].children[0].categoryNo);
 
-            var list = xhr.responseText;
-            console.log(typeof(list));
-            console.log(list);
+            var menu1 = document.getElementById("menu-1");
+            menu1.value = categories.categoryResponses[0].categoryNo;
+
+            var menu10 = document.getElementById("menu-10");
+            menu10.value = categories.categoryResponses[0].children[0].categoryNo;
+
+            var menu11 = document.getElementById("menu-11");
+            menu11.value = categories.categoryResponses[0].children[0].children[0].categoryNo;
+
+            var menu12 = document.getElementById("menu-12");
+            menu12.value = categories.categoryResponses[1].categoryNo;
+
+            var menu13 = document.getElementById("menu-13");
+            menu13.value = categories.categoryResponses[1].children[0].categoryNo;
         }
-        a = JSON.parse(this.responseText);
-        console.log("ㅊㅊㅊㅊㅊ",a.categoryResponses[0].children);
-    }
 
+    }
     xhr.send();
 
-    document.getElementById("menu1").addEventListener("click", openMenu1);
-    document.getElementById("menu12").addEventListener("click", openMenu12);
-    document.getElementById("menu10").addEventListener("click", openMenu10);
+    document.getElementById("menu-1").addEventListener("click", openMenu1);
+    document.getElementById("menu-12").addEventListener("click", openMenu12);
+    document.getElementById("menu-10").addEventListener("click", openMenu10);
 
     function openMenu1(){
         document.getElementById("dropdown").classList.toggle("active");
